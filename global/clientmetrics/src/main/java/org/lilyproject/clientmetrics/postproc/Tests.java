3ee7113da8157fa35b9f2f3bbfe86a5ca23051ca
Bruno Dumon <bruno@ngdata.com>
2011-01-17 12:25:32 +0000

    Add class forgotten in previous commit
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/trunk@4571 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ global/clientmetrics/src/main/java/org/lilyproject/clientmetrics/postproc/Tests.java
@@ -0,0 +1,12 @@
+package org.lilyproject.clientmetrics.postproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tests {
+    public List<Test> entries = new ArrayList<Test>();
+
+    public List<String> header = new ArrayList<String>();
+
+    public List<String> footer = new ArrayList<String>();
+}
