4dbd37a4e183d4643199a28319fcef0dc620ae19
Bruno Dumon <bruno@ngdata.com>
2011-03-29 08:32:48 +0000

    Add repo-test-fw, forgotten to add two commits ago.
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/trunk@4731 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ cr/repo-test-fw/src/main/java/org/lilyproject/repotestfw/ManualProcessRowLog.java
@@ -0,0 +1,108 @@
+package org.lilyproject.repotestfw;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.lilyproject.rowlog.api.*;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * A RowLog implementation that delegates to another RowLog and allows to manually trigger the processing
+ * of the messages put since the last manual trigger. For use in test cases.
+ */
+public class ManualProcessRowLog implements RowLog {
+    private RowLog delegate;
+    private List<RowLogMessage> unprocessedMessages = new ArrayList<RowLogMessage>();
+
+    public ManualProcessRowLog(RowLog delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public String getId() {
+        return delegate.getId();
+    }
+
+    @Override
+    public void registerShard(RowLogShard shard) {
+        delegate.registerShard(shard);
+    }
+
+    @Override
+    public void unRegisterShard(RowLogShard shard) {
+        delegate.unRegisterShard(shard);
+    }
+
+    @Override
+    public byte[] getPayload(RowLogMessage message) throws RowLogException {
+        return delegate.getPayload(message);
+    }
+
+    @Override
+    public RowLogMessage putMessage(byte[] rowKey, byte[] data, byte[] payload, Put put) throws RowLogException,
+            InterruptedException {
+
+        RowLogMessage msg = delegate.putMessage(rowKey, data, payload, put);
+        unprocessedMessages.add(msg);
+
+        return msg;
+    }
+
+    public void processMessages() throws RowLogException, InterruptedException {
+        while (!unprocessedMessages.isEmpty()) {
+            RowLogMessage msg = unprocessedMessages.remove(0);
+            processMessage(msg, null);
+        }
+    }
+
+    @Override
+    public boolean processMessage(RowLogMessage message, Object lock) throws RowLogException, InterruptedException {
+        return delegate.processMessage(message, lock);
+    }
+
+    @Override
+    public Object lockMessage(RowLogMessage message, String subscriptionId) throws RowLogException {
+        return delegate.lockMessage(message, subscriptionId);
+    }
+
+    @Override
+    public boolean unlockMessage(RowLogMessage message, String subscriptionId, Object lock) throws RowLogException {
+        return delegate.unlockMessage(message, subscriptionId, lock);
+    }
+
+    @Override
+    public boolean isMessageLocked(RowLogMessage message, String subscriptionId) throws RowLogException {
+        return delegate.isMessageLocked(message, subscriptionId);
+    }
+
+    @Override
+    public boolean messageDone(RowLogMessage message, String subscriptionId, Object lock) throws RowLogException {
+        return delegate.messageDone(message, subscriptionId, lock);
+    }
+
+    @Override
+    public boolean isMessageDone(RowLogMessage message, String subscriptionId) throws RowLogException {
+        return delegate.isMessageDone(message, subscriptionId);
+    }
+
+    @Override
+    public List<RowLogMessage> getMessages(byte[] rowKey, String... subscriptionId) throws RowLogException {
+        return delegate.getMessages(rowKey, subscriptionId);
+    }
+
+    @Override
+    public List<RowLogSubscription> getSubscriptions() {
+        return delegate.getSubscriptions();
+    }
+
+    @Override
+    public List<RowLogShard> getShards() {
+        return delegate.getShards();
+    }
+
+    @Override
+    public boolean isMessageAvailable(RowLogMessage message, String subscriptionId) throws RowLogException {
+        return delegate.isMessageAvailable(message, subscriptionId);
+    }
+}
---
+++ cr/repo-test-fw/src/main/java/org/lilyproject/repotestfw/RepositorySetup.java
@@ -0,0 +1,314 @@
+package org.lilyproject.repotestfw;
+
+import org.apache.avro.ipc.NettyServer;
+import org.apache.avro.ipc.Server;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.junit.Assert;
+import org.lilyproject.repository.api.*;
+import org.lilyproject.repository.avro.AvroConverter;
+import org.lilyproject.repository.avro.AvroLily;
+import org.lilyproject.repository.avro.AvroLilyImpl;
+import org.lilyproject.repository.avro.LilySpecificResponder;
+import org.lilyproject.repository.impl.*;
+import org.lilyproject.rowlock.RowLocker;
+import org.lilyproject.rowlog.api.*;
+import org.lilyproject.rowlog.impl.*;
+import org.lilyproject.testfw.HBaseProxy;
+import org.lilyproject.util.hbase.HBaseTableFactory;
+import org.lilyproject.util.hbase.HBaseTableFactoryImpl;
+import org.lilyproject.util.hbase.LilyHBaseSchema;
+import org.lilyproject.util.hbase.LilyHBaseSchema.RecordCf;
+import org.lilyproject.util.hbase.LilyHBaseSchema.RecordColumn;
+import org.lilyproject.util.io.Closer;
+import org.lilyproject.util.zookeeper.ZkUtil;
+import org.lilyproject.util.zookeeper.ZooKeeperItf;
+
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Helper class to instantiate and wire all the repository related services.
+ */
+public class RepositorySetup {
+    private HBaseProxy hbaseProxy = new HBaseProxy();
+    private Configuration hadoopConf;
+    private ZooKeeperItf zk;
+
+    private HBaseTableFactory hbaseTableFactory;
+
+    private RowLogConfigurationManagerImpl rowLogConfManager;
+
+    private IdGenerator idGenerator;
+    private HBaseTypeManager typeManager;
+    private RemoteTypeManager remoteTypeManager;
+    private HBaseRepository repository;
+    private RemoteRepository remoteRepository;
+
+    private Server lilyServer;
+
+    private BlobStoreAccessFactory blobStoreAccessFactory;
+    private BlobStoreAccessFactory remoteBlobStoreAccessFactory;
+
+    private BlobManager blobManager;
+    private BlobManager remoteBlobManager;
+
+    private RowLog wal;
+    private RowLog mq;
+
+    private RowLogProcessorImpl mqProcessor;
+
+    private boolean coreSetup;
+    private boolean typeManagerSetup;
+    private boolean repositorySetup;
+
+    private long hbaseBlobLimit = -1;
+    private long inlineBlobLimit = -1;
+
+    public void setupCore() throws Exception {
+        if (coreSetup)
+            return;
+        hbaseProxy.start();
+        hadoopConf = hbaseProxy.getConf();
+        zk = ZkUtil.connect(hbaseProxy.getZkConnectString(), 10000);
+
+        hbaseTableFactory = new HBaseTableFactoryImpl(hadoopConf);
+
+        coreSetup = true;
+    }
+
+    public void setupTypeManager() throws Exception {
+        if (typeManagerSetup)
+            return;
+
+        idGenerator = new IdGeneratorImpl();
+        typeManager = new HBaseTypeManager(idGenerator, hadoopConf, zk, hbaseTableFactory);
+
+        typeManagerSetup = true;
+    }
+
+    public void setupRepository(boolean withWal) throws Exception {
+        if (repositorySetup)
+            return;
+
+        setupTypeManager();
+
+        blobStoreAccessFactory = createBlobAccess();
+        blobManager = new BlobManagerImpl(hbaseTableFactory, blobStoreAccessFactory, false);
+
+        if (withWal) {
+            setupRowLogConfigurationManager();
+            RowLocker rowLocker = new RowLocker(LilyHBaseSchema.getRecordTable(hbaseTableFactory), RecordCf.DATA.bytes, RecordColumn.LOCK.bytes, 10000);
+            rowLogConfManager.addRowLog("WAL", new RowLogConfig(10000L, true, false, 100L, 5000L, 5000L));
+            wal = new RowLogImpl("WAL", LilyHBaseSchema.getRecordTable(hbaseTableFactory), LilyHBaseSchema.RecordCf.ROWLOG.bytes,
+                    LilyHBaseSchema.RecordColumn.WAL_PREFIX, rowLogConfManager, rowLocker);
+            RowLogShard walShard = new RowLogShardImpl("WS1", hadoopConf, wal, 100);
+            wal.registerShard(walShard);
+        }
+
+        repository = new HBaseRepository(typeManager, idGenerator, wal, hadoopConf, hbaseTableFactory, blobManager);
+
+        repositorySetup = true;
+    }
+
+    private BlobStoreAccessFactory createBlobAccess() throws Exception {
+        DFSBlobStoreAccess dfsBlobStoreAccess = new DFSBlobStoreAccess(hbaseProxy.getBlobFS(), new Path("/lily/blobs"));
+        BlobStoreAccess hbaseBlobStoreAccess = new HBaseBlobStoreAccess(hadoopConf);
+        BlobStoreAccess inlineBlobStoreAccess = new InlineBlobStoreAccess();
+
+        BlobStoreAccessConfig blobStoreAccessConfig = new BlobStoreAccessConfig(dfsBlobStoreAccess.getId());
+
+        if (hbaseBlobLimit != -1) {
+            blobStoreAccessConfig.setLimit(hbaseBlobStoreAccess.getId(), hbaseBlobLimit);
+        }
+
+        if (inlineBlobLimit != -1) {
+            blobStoreAccessConfig.setLimit(inlineBlobStoreAccess.getId(), inlineBlobLimit);
+        }
+
+        List<BlobStoreAccess> blobStoreAccesses = Arrays.asList(new BlobStoreAccess[]{dfsBlobStoreAccess, hbaseBlobStoreAccess, inlineBlobStoreAccess});
+        SizeBasedBlobStoreAccessFactory blobStoreAccessFactory = new SizeBasedBlobStoreAccessFactory(blobStoreAccesses, blobStoreAccessConfig);
+        return blobStoreAccessFactory;
+    }
+
+    /**
+     * Set the size limits for the inline and HBase blobs, set to -1 to disable one of these
+     * stores.
+     */
+    public void setBlobLimits(long inlineBlobLimit, long hbaseBlobLimit) {
+        this.inlineBlobLimit = inlineBlobLimit;
+        this.hbaseBlobLimit = hbaseBlobLimit;
+    }
+
+    public void setupRowLogConfigurationManager() throws Exception {
+        rowLogConfManager = new RowLogConfigurationManagerImpl(zk);
+    }
+
+    public void setupRemoteAccess() throws Exception {
+        AvroConverter serverConverter = new AvroConverter();
+        serverConverter.setRepository(repository);
+        lilyServer = new NettyServer(
+                new LilySpecificResponder(AvroLily.class, new AvroLilyImpl(repository, serverConverter),
+                        serverConverter), new InetSocketAddress(0));
+        lilyServer.start();
+
+        AvroConverter remoteConverter = new AvroConverter();
+
+        remoteTypeManager = new RemoteTypeManager(new InetSocketAddress(lilyServer.getPort()),
+                remoteConverter, idGenerator, zk);
+
+        remoteBlobStoreAccessFactory = createBlobAccess();
+        remoteBlobManager = new BlobManagerImpl(hbaseTableFactory, remoteBlobStoreAccessFactory, false);
+
+        remoteRepository = new RemoteRepository(new InetSocketAddress(lilyServer.getPort()), remoteConverter,
+                remoteTypeManager, idGenerator, remoteBlobManager);
+
+        remoteConverter.setRepository(repository);
+        remoteTypeManager.start();
+
+    }
+
+    public void setupMessageQueue(boolean withProcessor) throws Exception {
+        setupMessageQueue(withProcessor, false);
+    }
+
+    /**
+     *
+     * @param withManualProcessing if true, the MQ RowLog will be wrapped to keep track of added messages to allow
+     *                             triggering a manual processing, see method {@processMQ}. Usually you will want
+     *                             either this or withProcessor, not both.
+     */
+    public void setupMessageQueue(boolean withProcessor, boolean withManualProcessing) throws Exception {
+
+        rowLogConfManager.addRowLog("MQ", new RowLogConfig(10000L, false, true, 100L, 0L, 5000L));
+        rowLogConfManager.addSubscription("WAL", "MQFeeder", RowLogSubscription.Type.VM, 1);
+
+        mq = new RowLogImpl("MQ", LilyHBaseSchema.getRecordTable(hbaseTableFactory), LilyHBaseSchema.RecordCf.ROWLOG.bytes,
+                LilyHBaseSchema.RecordColumn.MQ_PREFIX, rowLogConfManager, null);
+        if (withManualProcessing) {
+            mq = new ManualProcessRowLog(mq);
+        }
+        mq.registerShard(new RowLogShardImpl("MQS1", hadoopConf, mq, 100));
+
+        RowLogMessageListenerMapping listenerClassMapping = RowLogMessageListenerMapping.INSTANCE;
+        listenerClassMapping.put("MQFeeder", new MessageQueueFeeder(mq));
+
+        waitForSubscription(wal, "MQFeeder");
+
+        if (withProcessor) {
+            mqProcessor = new RowLogProcessorImpl(mq, rowLogConfManager);
+            mqProcessor.start();
+        }
+    }
+
+    /**
+     * When the message queue was setup with the option for manuall processing, calling this method will
+     * trigger synchronous MQ processing.
+     */
+    public void processMQ() throws RowLogException, InterruptedException {
+        ((ManualProcessRowLog)mq).processMessages();
+    }
+
+    public void stop() throws InterruptedException {
+        if (mqProcessor != null)
+            mqProcessor.stop();
+
+        Closer.close(remoteTypeManager);
+        Closer.close(remoteRepository);
+
+        Closer.close(typeManager);
+        Closer.close(repository);
+
+        if (lilyServer != null) {
+            lilyServer.close();
+            lilyServer.join();
+        }
+
+        Closer.close(rowLogConfManager);
+
+        Closer.close(zk);
+        Closer.close(hbaseProxy);
+        coreSetup = false;
+        repositorySetup = false;
+        typeManagerSetup = false;
+    }
+
+    public void waitForSubscription(RowLog rowLog, String subscriptionId) throws InterruptedException {
+        boolean subscriptionKnown = false;
+        int timeOut = 10000;
+        long waitUntil = System.currentTimeMillis() + timeOut;
+        while (!subscriptionKnown && System.currentTimeMillis() < waitUntil) {
+            for (RowLogSubscription subscription : rowLog.getSubscriptions()) {
+                if (subscriptionId.equals(subscription.getId())) {
+                    subscriptionKnown = true;
+                    break;
+                }
+            }
+            Thread.sleep(10);
+        }
+        Assert.assertTrue("Subscription '" + subscriptionId + "' not known to rowlog within timeout " + timeOut + "ms",
+                subscriptionKnown);
+    }
+
+    public ZooKeeperItf getZk() {
+        return zk;
+    }
+
+    public TypeManager getRemoteTypeManager() {
+        return remoteTypeManager;
+    }
+
+    public Repository getRepository() {
+        return repository;
+    }
+
+    public Repository getRemoteRepository() {
+        return remoteRepository;
+    }
+
+    public TypeManager getTypeManager() {
+        return typeManager;
+    }
+
+    public IdGenerator getIdGenerator() {
+        return idGenerator;
+    }
+
+    public RowLog getWal() {
+        return wal;
+    }
+
+    public RowLog getMq() {
+        return mq;
+    }
+
+    public Configuration getHadoopConf() {
+        return hadoopConf;
+    }
+
+    public HBaseTableFactory getHbaseTableFactory() {
+        return hbaseTableFactory;
+    }
+
+    public RowLogConfigurationManagerImpl getRowLogConfManager() {
+        return rowLogConfManager;
+    }
+
+    public BlobStoreAccessFactory getBlobStoreAccessFactory() {
+        return blobStoreAccessFactory;
+    }
+
+    public BlobStoreAccessFactory getRemoteBlobStoreAccessFactory() {
+        return remoteBlobStoreAccessFactory;
+    }
+
+    public BlobManager getBlobManager() {
+        return blobManager;
+    }
+
+    public BlobManager getRemoteBlobManager() {
+        return remoteBlobManager;
+    }
+
+}
