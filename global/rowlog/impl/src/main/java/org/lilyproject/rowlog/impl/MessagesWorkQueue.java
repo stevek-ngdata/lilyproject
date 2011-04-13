a7198c6f51fe35e199eff03bc7ed6a4273a762be
Evert Arckens <evert@ngdata.com>
2011-04-13 12:12:03 +0000

    Changed the MessagesWorkQueue so that it doesn't drop messages when the same row is being worked on by another listener. Instead it skips it until it finds a message that can be worked on. Also no longer using a BlockingQueue, but rather a wait/notify mechanism on a list.
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/trunk@4772 b80d95f8-e103-0410-bd55-e03a9dc29394

--- global/rowlog/impl/src/main/java/org/lilyproject/rowlog/impl/MessagesWorkQueue.java
+++ global/rowlog/impl/src/main/java/org/lilyproject/rowlog/impl/MessagesWorkQueue.java
@@ -15,17 +15,16 @@
  */
 package org.lilyproject.rowlog.impl;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.*;
 
 import org.lilyproject.rowlog.api.RowLogMessage;
 import org.lilyproject.util.ByteArrayKey;
 
 public class MessagesWorkQueue {
-    private BlockingQueue<RowLogMessage> messageQueue = new ArrayBlockingQueue<RowLogMessage>(100);
+private static final int MAX_MESSAGES = 100;
 
+    private List<RowLogMessage> messageList = new ArrayList<RowLogMessage>(MAX_MESSAGES);
+    
     private Set<RowLogMessage> messagesWorkingOn = new HashSet<RowLogMessage>();
     private Set<ByteArrayKey> rowsWorkingOn = new HashSet<ByteArrayKey>();
     
@@ -37,60 +36,62 @@ public class MessagesWorkQueue {
     private final Object refillTrigger = new Object();
 
     public void offer(RowLogMessage message) throws InterruptedException {
-        // First check if the message is already in the queue or is being worked on
-        synchronized(messagesWorkingOn) {
-            if (messageQueue.contains(message) || messagesWorkingOn.contains(message)) 
-                return;
+        synchronized (messageList) {
+            if (messageList.size() >= MAX_MESSAGES) {
+                messageList.wait();
+                offer(message);
+            } else {
+                messageList.add(message);
+                messageList.notifyAll();
+            }
         }
-        // Put the message on the queue, this blocks until there is room on the messageQueue
-        // This can(should) be done outside the synchronize block sine #offer is only called from one place (one thread)
-        // so no one else can jump in between.
-        messageQueue.put(message);
     }
     
     public RowLogMessage take() throws InterruptedException {
-        // Take a message from the queue, this blocks until there is a message on the queue
-        RowLogMessage message = messageQueue.take();
-        // Only add new messages to the queue when the number of messages in the queue drops below the threshold
-        if (messageQueue.size() <= refillThreshold) {
-            synchronized (refillTrigger) {
-                refillTrigger.notifyAll();
+        synchronized (messageList) {
+            if (messageList.size() == 0) {
+                messageList.wait();
+                return take();
+            } else {
+                Iterator<RowLogMessage> messages = messageList.iterator();
+                while (messages.hasNext()) {
+                    RowLogMessage message = messages.next();
+                    ByteArrayKey row = new ByteArrayKey(message.getRowKey());
+                    if (messagesWorkingOn.contains(message)) {
+                        messages.remove();
+                    } else if (!rowsWorkingOn.contains(row)) {
+                        messages.remove();
+                        messagesWorkingOn.add(message);
+                        rowsWorkingOn.add(row);
+                        messageList.notifyAll();
+                        if (messageList.size() <= refillThreshold) {
+                            synchronized (refillTrigger) {
+                                refillTrigger.notifyAll();
+                            }
+                        }
+                        return message;
+                    }
+                }
+                messageList.wait();
+                return take();
             }
-        }
-
-        // Make sure no other listener is working on the message or on the same row
-        synchronized (messagesWorkingOn) {
-            // In case the message was added again before we could synchronize on messagesWorkingOn,
-            // remove it again to avoid that another listener would start working on it
-            // Note : we can still be too late, so we need to check next it again (below)
-            messageQueue.remove(message); 
-            ByteArrayKey rowKey = new ByteArrayKey(message.getRowKey());
-            // Make sure the message was not added again (before synchronizing) and another listener started working on it
-            // And also check that no other listener is working on the same row
-            if (messagesWorkingOn.contains(message) || rowsWorkingOn.contains(rowKey)) 
-                return null; 
-            // The message can be returned and marked that it and its row are going to be worked on
-            messagesWorkingOn.add(message);
-            rowsWorkingOn.add(rowKey);
-            return message;
         }
     }
     
     public void done(RowLogMessage message) {
-        // Done with the message, safely remove it and its row
-        synchronized (messagesWorkingOn) {
+        synchronized (messageList) {
             messagesWorkingOn.remove(message);
-            rowsWorkingOn.remove(new ByteArrayKey(message.getRowKey()));
+            rowsWorkingOn.remove(new ByteArrayKey(message.getRowKey()));            
         }
     }
     
     public int size() {
-    	return messageQueue.size();
+    	return messageList.size();
     }
 
     public void waitOnRefillThreshold() throws InterruptedException {
         synchronized (refillTrigger) {
-            while (messageQueue.size() > refillThreshold) {
+            while (messageList.size() > refillThreshold) {
                 refillTrigger.wait();
             }
         }
