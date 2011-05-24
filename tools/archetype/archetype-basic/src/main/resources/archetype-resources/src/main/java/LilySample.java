9a5014ddabec4b5e19d9cee40daffca5d1f4c8ea
Bruno Dumon <bruno@ngdata.com>
2011-05-24 15:54:25 +0000

    Add a maven archetype to generate a maven project for your own Lily-based application.
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/trunk@4880 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ tools/archetype/archetype-basic/src/main/resources/archetype-resources/src/main/java/LilySample.java
@@ -0,0 +1,51 @@
+package ${package};
+
+import org.lilyproject.client.LilyClient;
+import org.lilyproject.repository.api.*;
+import org.lilyproject.tools.import_.cli.JsonImport;
+
+import java.io.InputStream;
+
+public class LilySample {
+    public static void main(String[] args) throws Exception {
+        new LilySample().run();
+    }
+
+    public void run() throws Exception {
+        //
+        // Instantiate Lily client
+        //
+        LilyClient lilyClient = new LilyClient("localhost:2181", 20000);
+        Repository repository = lilyClient.getRepository();
+
+        //
+        // Create a schema
+        //
+        // We do this using the import library: this is easier than writing
+        // out the schema construction code in Java, and automatically handles
+        // the case where the schema would already exist (on second run of this app).
+        //
+        System.out.println("Importing schema");
+        InputStream is = LilySample.class.getResourceAsStream("schema.json");
+        JsonImport.load(repository, is, false);
+        is.close();
+        System.out.println("Schema successfully imported");
+
+        //
+        // Create a record
+        //
+        System.out.println("Creating a record");
+        Record record = repository.newRecord();
+        record.setId(repository.getIdGenerator().newRecordId());
+        record.setRecordType(q("Type1"));
+        record.setField(q("field1"), "value1");
+        // We use the createOrUpdate method as that one can automatically recover
+        // from connection errors (idempotent behavior, like PUT in HTTP).
+        repository.createOrUpdate(record);
+        System.out.println("Record created: " + record.getId());
+    }
+
+    private static QName q(String name) {
+        return new QName("${package}", name);
+    }
+}
