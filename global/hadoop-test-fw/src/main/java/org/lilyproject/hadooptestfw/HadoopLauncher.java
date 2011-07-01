6df1aeba9a424661b56b7b0895d936aaa68dd541
Bruno Dumon <bruno@ngdata.com>
2011-07-01 15:54:59 +0000

    Re-introduce launch-hadoop command for usage during Lily development. The idea is that while working on Lily, the complete Lily tree might not build (and lily-server-test-fw depends on almost everything), and even if it builds it might not start, removing the ability to run tests in connect mode then. Therefore, having launch-hadoop along with launch-test-lily still makes sense.
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/trunk@4978 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ global/hadoop-test-fw/src/main/java/org/lilyproject/hadooptestfw/HadoopLauncher.java
@@ -0,0 +1,74 @@
+package org.lilyproject.hadooptestfw;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.lilyproject.cli.BaseCliTool;
+import org.lilyproject.hadooptestfw.fork.HBaseTestingUtility;
+import org.lilyproject.util.test.TestHomeUtil;
+
+import java.io.File;
+
+/**
+ * Utility to easily launch a full HBase with a temporary storage. Intended to be used to run testcases
+ * against (see HBaseProxy connect mode).
+ */
+public class HadoopLauncher extends BaseCliTool {
+    private File baseTempDir;
+    private Configuration conf;
+
+    @Override
+    protected String getCmdName() {
+        return "launch-hadoop";
+    }
+
+    public static void main(String[] args) throws Exception {
+        new HadoopLauncher().start(args);
+    }
+
+    @Override
+    public int run(CommandLine cmd) throws Exception {
+        int result = super.run(cmd);
+        if (result != 0)
+            return result;
+
+        TestHelper.setupConsoleLogging("INFO");
+        TestHelper.setupOtherDefaults();
+
+        conf = HBaseConfiguration.create();
+
+        baseTempDir = TestHomeUtil.createTestHome("launch-hadoop-");
+
+        HBaseTestingUtility testUtil = HBaseTestingUtilityFactory.create(conf, baseTempDir);
+        testUtil.startMiniCluster(1);
+        testUtil.startMiniMapReduceCluster(1);
+
+        System.out.println("-------------------------");
+        System.out.println("Minicluster is up");
+        System.out.println();
+        System.out.println("To connect to this HBase, use the following properties:");
+        System.out.println("hbase.zookeeper.quorum=localhost");
+        System.out.println("hbase.zookeeper.property.clientPort=2181");
+        System.out.println();
+        System.out.println("In Java code, create the HBase configuration like this:");
+        System.out.println("Configuration conf = HBaseConfiguration.create();");
+        System.out.println("conf.set(\"hbase.zookeeper.quorum\", \"localhost\");");
+        System.out.println("conf.set(\"hbase.zookeeper.property.clientPort\", \"2181\");");
+        System.out.println();
+        System.out.println("For MapReduce, use:");
+        System.out.println("Configuration conf = new Configuration();");
+        System.out.println("conf.set(\"mapred.job.tracker\", \"localhost:" +
+                testUtil.getMRCluster().getJobTrackerPort() + "\");");
+        System.out.println("Job job = new Job(conf);");
+        System.out.println();
+        System.out.println("JobTracker web ui:   http://localhost:" +
+                testUtil.getMRCluster().getJobTrackerRunner().getJobTrackerInfoPort());
+        System.out.println("HDFS web ui:         http://" + conf.get("dfs.http.address"));
+        System.out.println("HBase master web ui: http://localhost:" +
+                testUtil.getHBaseCluster().getMaster().getInfoServer().getPort());
+        System.out.println("-------------------------");
+
+        return 0;
+    }
+
+}
