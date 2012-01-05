aac160bcdb35b8bb3bfda3e02f8eec412882839c
Bruno Dumon <bruno@ngdata.com>
2012-01-05 17:34:18 +0000

    Introducing integration tests for checking some resetLilyState behavior.
    resetLilyState can only be tested when using launch-test-lily, we can't just launch that from the test as it would conflict with launch-hadoop when using the connect mode. Also, as these kinds of tests can be time consuming, it makes sense to separate them from the ordinary tests.
    
    Therefore, run this test as an integration test (maven failsafe plugin).
    
    Starting and stopping launch-test-lily is performed by shell scripts invoked from the pre/post-integration-test maven phases.
    
    The integration tests are not run by default: use mvn -Pintegration install
    
    
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/branches/BRANCH_1_1_X@5312 b80d95f8-e103-0410-bd55-e03a9dc29394

---
+++ integration-tests/reset-lily-state/start-launch-test-lily.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#
+# This script starts launch-test-lily and waits for it to be fully started
+#
+
+set -e
+set -u
+
+# Check no other/previous instance is running
+PORTCHECK=$(netstat -l | grep 8020 || true)
+if [ -n "$PORTCHECK" ]; then
+  echo launch-test-lily or launch-hadoop or other hadoop is already running
+  exit 1
+fi
+
+COMMAND=$1/../../cr/standalone-launcher/target/launch-test-lily
+
+OUTPUT=/tmp/start-launch-test-lily-output
+
+# Start it
+$COMMAND > $OUTPUT 2>&1 &
+
+# Wait until its fully started
+echo Waiting for launch-test-lily to be started
+started=`date +%s`
+sleep 5
+while [ -z "$(grep 'Lily is running' $OUTPUT)" ]
+do
+  now=`date +%s`
+  elapsed=$((now - started))
+  if [[ $elapsed  -gt 600 ]] ; then
+    echo "Did not succeed within timeout, quiting"
+    echo "Check $OUTPUT"
+    exit 1
+  fi
+  sleep 2
+done
+echo launch-test-lily started
