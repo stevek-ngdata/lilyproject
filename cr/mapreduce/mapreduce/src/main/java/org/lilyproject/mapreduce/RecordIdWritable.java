35d1d3777f7eef1629abbbe0bd7b14d6a9f1bed2
Bruno Dumon <bruno@ngdata.com>
2012-03-06 14:40:20 +0000

    Finish the RecordIdWritable implementation
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/branches/scanners@5426 b80d95f8-e103-0410-bd55-e03a9dc29394

--- cr/mapreduce/mapreduce/src/main/java/org/lilyproject/mapreduce/RecordIdWritable.java
+++ cr/mapreduce/mapreduce/src/main/java/org/lilyproject/mapreduce/RecordIdWritable.java
@@ -1,29 +1,78 @@
 package org.lilyproject.mapreduce;
 
+import org.apache.hadoop.io.BinaryComparable;
 import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableUtils;
+import org.lilyproject.repository.api.IdGenerator;
 import org.lilyproject.repository.api.RecordId;
+import org.lilyproject.repository.impl.id.IdGeneratorImpl;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class RecordIdWritable implements WritableComparable<RecordIdWritable> {
+/**
+ * A Hadoop WritableComparable for Lily RecordId's.
+ *
+ * <p>Stores the length of the record id as a vint, followed by the normal binary
+ * representation of the record id (= the same as used as row key in the HBase table).</p>
+ */
+public class RecordIdWritable extends BinaryComparable implements WritableComparable<BinaryComparable> {
     private RecordId recordId;
-
-    @Override
-    public int compareTo(RecordIdWritable o) {
-        // TODO
-        return 0;
+    private static final IdGenerator ID_GENERATOR = new IdGeneratorImpl();
+    
+    public RecordIdWritable() {        
+    }
+    
+    public RecordIdWritable(RecordId recordId) {
+        this.recordId = recordId;
     }
 
     @Override
-    public void write(DataOutput dataOutput) throws IOException {
-        // TODO
+    public int getLength() {
+        // RecordId's cache their binary representation, so this should be fast
+        return recordId.toBytes().length;
     }
 
     @Override
-    public void readFields(DataInput dataInput) throws IOException {
-        // TODO
+    public byte[] getBytes() {
+        return recordId.toBytes();
+    }
+
+    // Disclaimer: the Comparator was copied from Hadoop's Text class.
+    public static class Comparator extends WritableComparator {
+        public Comparator() {
+            super(RecordIdWritable.class);
+        }
+
+        public int compare(byte[] b1, int s1, int l1,
+                byte[] b2, int s2, int l2) {
+            // don't include the vint length in the comparison
+            int n1 = WritableUtils.decodeVIntSize(b1[s1]);
+            int n2 = WritableUtils.decodeVIntSize(b2[s2]);
+            return compareBytes(b1, s1+n1, l1-n1, b2, s2+n2, l2-n2);
+        }
+    }
+
+    static {
+        // register this comparator
+        WritableComparator.define(RecordIdWritable.class, new Comparator());
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        byte[] bytes = recordId.toBytes();
+        WritableUtils.writeVInt(out, bytes.length);
+        out.write(bytes);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        int length = WritableUtils.readVInt(in);
+        byte[] bytes = new byte[length];
+        in.readFully(bytes, 0, length);
+        this.recordId = ID_GENERATOR.fromBytes(bytes);
     }
 
     public RecordId getRecordId() {
