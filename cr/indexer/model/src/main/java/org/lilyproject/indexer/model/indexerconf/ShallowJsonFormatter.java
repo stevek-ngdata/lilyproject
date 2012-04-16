7f36bcab3b384e5156258e3a145c724400c3c719
Karel Vervaeke <karel@ngdata.com>
2012-04-16 12:23:44 +0000

    Fix copyright annotation
    git-svn-id: svn+ssh://dev.outerthought.org/outerthought/lilyproject/trunk@5538 b80d95f8-e103-0410-bd55-e03a9dc29394

--- cr/indexer/model/src/main/java/org/lilyproject/indexer/model/indexerconf/ShallowJsonFormatter.java
+++ cr/indexer/model/src/main/java/org/lilyproject/indexer/model/indexerconf/ShallowJsonFormatter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Alex Shumilov
+ * Copyright 2012 NGDATA nv
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
