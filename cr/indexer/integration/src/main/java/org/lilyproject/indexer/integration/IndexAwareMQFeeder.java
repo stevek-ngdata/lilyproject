30e4e45409e37e23913de7214f5c4124c015350b
Bruno Dumon <bruno@ngdata.com>
2012-07-16 16:44:20 +0200

    Do the lily.mq=false check at the start of processMessage, refactor code into different methods.

--- cr/indexer/integration/src/main/java/org/lilyproject/indexer/integration/IndexAwareMQFeeder.java
+++ cr/indexer/integration/src/main/java/org/lilyproject/indexer/integration/IndexAwareMQFeeder.java
@@ -17,6 +17,7 @@ package org.lilyproject.indexer.integration;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.lilyproject.indexer.model.indexerconf.IndexRecordFilter;
 import org.lilyproject.indexer.model.util.IndexInfo;
 import org.lilyproject.indexer.model.util.IndexesInfo;
@@ -60,116 +61,145 @@ public class IndexAwareMQFeeder implements RowLogMessageListener {
 
     @Override
     public boolean processMessage(RowLogMessage message) throws InterruptedException {
-        //
+        // Load & parse RecordEvent
+        RecordEvent recordEvent = getRecordEvent(message);
+        if (recordEvent == null) {
+            return true;
+        }
+
+        // Don't feed MQ if attribute lily.mq=false is set
+        if ("false".equals(recordEvent.getAttributes().get("lily.mq"))) {
+            return true;
+        }
+
         // Get a snapshot of the currently known MQ subscriptions
-        //
         List<RowLogSubscription> subscriptions = new ArrayList<RowLogSubscription>(messageQueue.getSubscriptions());
 
-        //
         // If there are indexes, remove their subscription from the list if we see the record is not
         // relevant for the index. This avoids the cost of needlessly dispatching this event to
         // indexes that don't need it anyway.
-        //
         Collection<IndexInfo> indexInfos = indexesInfo.getIndexInfos();
         if (indexInfos.size() >= 0) {
-            try {
-                //
-                // Get the RecordEvent object (optimized to avoid re-parsing)
-                //
-                Object context = message.getContext();
-                RecordEvent recordEvent = null;
-                if (context != null) {
-                    RowLogContext rowLogContext = (RowLogContext)message.getContext();
-                    recordEvent = rowLogContext.getRecordEvent();
-                }
-                if (recordEvent == null) {
-                    recordEvent = new RecordEvent(message.getPayload(), repository.getIdGenerator());
-                }
-                
-                // Check the attributes on the recordEvent to make sure that the event should be processed.
-                boolean isRecordProcessable = !"false".equals(recordEvent.getAttributes().get("lily.mq"));
-                if (!isRecordProcessable) {
-                    // The record should not be processed so we will clear the subscriptions                 
-                    subscriptions.clear();
-                } else {
-
-                    //
-                    // Create the 'old' and 'new' Record instances.
-                    //
-                    // The RecordEvent contains the fields & record type info needed by the filters of the different
-                    // indexes, this is taken care of by IndexSelectionRecordUpdateHook.
-                    //
-                    // Of course, it can happen that indexerconfs have been changed, or new indexes have been added,
-                    // since the event was created, and then this information will be missing. We could check for that
-                    // and in case of doubt send the event to the index anyway. This approach however also has the
-                    // disadvantage that, in case there are a lot of outstanding events in the queue, that they
-                    // might be sent to indexes that only expect a low update rate. Besides, it also complicates the
-                    // code. So we go for the simple approach: when the indexerconfs change, there is a transition
-                    // period to be expected, and one might need to rebuild indexes.
-                    //
-                    RecordId recordId = repository.getIdGenerator().fromBytes(message.getRowKey());
-                    Record newRecord = repository.newRecord(recordId);
-                    Record oldRecord = repository.newRecord(recordId);
-    
-                    RecordEvent.IndexSelection idxSel = recordEvent.getIndexSelection();
-                    if (idxSel != null) {
-                        TypeManager typeManager = repository.getTypeManager();
-    
-                        if (idxSel.getFieldChanges() != null) {
-                            for (RecordEvent.FieldChange fieldChange : idxSel.getFieldChanges()) {
-                                FieldType fieldType = typeManager.getFieldTypeById(fieldChange.getId());
-                                QName name = fieldType.getName();
-    
-                                if (fieldChange.getNewValue() != null) {
-                                    Object value = fieldType.getValueType().read(fieldChange.getNewValue());
-                                    newRecord.setField(name, value);
-                                }
-    
-                                if (fieldChange.getOldValue() != null) {
-                                    Object value = fieldType.getValueType().read(fieldChange.getOldValue());
-                                    oldRecord.setField(name, value);
-                                }
-                            }
-                        }
-    
-                        if (idxSel.getNewRecordType() != null) {
-                            newRecord.setRecordType(typeManager.getRecordTypeById(idxSel.getNewRecordType(), null).getName());
-                        }
-    
-                        if (idxSel.getOldRecordType() != null) {
-                            oldRecord.setRecordType(typeManager.getRecordTypeById(idxSel.getOldRecordType(), null).getName());
-                        }
-                    }
-    
-                    //
-                    // And now, the actual subscription filtering
-                    //
-                    for (IndexInfo indexInfo : indexInfos) {
-                        // If the filter of the indexerconf matches either the old or new record state,
-                        // then the index needs to process this event
-                        boolean relevantIndex = false;
-                        IndexRecordFilter filter = indexInfo.getIndexerConf().getRecordFilter();
-                        if (filter.getIndexCase(oldRecord) != null || filter.getIndexCase(newRecord) != null) {
-                            relevantIndex = true;
-                        }
-    
-                        // If not relevant, remove it from the list of subscriptions
-                        if (!relevantIndex) {
-                            String subscriptionId = indexInfo.getIndexDefinition().getQueueSubscriptionId();
-                            removeSubscription(subscriptions, subscriptionId);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                log.error("Error while performing index-aware filtering of subscriptions in the MQ feeder", e);
-            }
+            filterSubscriptions(subscriptions, indexInfos, message, recordEvent);
         }
 
-        //
-        // Add event to the MQ, this is the original code from MessageQueueFeeder with an additional
-        // argument to putMessage.
-        //
+        // Add event to the MQ
+        return addMessageToMQ(message, subscriptions);
+    }
 
+    private void removeSubscription(List<RowLogSubscription> subscriptions, String idToRemove) {
+        Iterator<RowLogSubscription> subscriptionIt = subscriptions.iterator();
+        while (subscriptionIt.hasNext()) {
+            if (subscriptionIt.next().getId().equals(idToRemove)) {
+                subscriptionIt.remove();
+                return;
+            }
+        }
+    }
+
+    private RecordEvent getRecordEvent(RowLogMessage message) {
+        // Get the RecordEvent object (optimized to avoid re-parsing)
+        Object context = message.getContext();
+        RecordEvent recordEvent = null;
+        if (context != null) {
+            RowLogContext rowLogContext = (RowLogContext)message.getContext();
+            recordEvent = rowLogContext.getRecordEvent();
+        }
+        if (recordEvent == null) {
+            try {
+                recordEvent = new RecordEvent(message.getPayload(), repository.getIdGenerator());
+            } catch (Exception e) {
+                log.error("Error loading or parsing record event for record " +
+                        Bytes.toStringBinary(message.getRowKey()), e);
+            }
+        }
+        return recordEvent;
+    }
+
+    /**
+     * Performs the index-aware filtering of the subscriptions, modifies the provided subscriptions
+     * list.
+     */
+    private void filterSubscriptions(List<RowLogSubscription> subscriptions, Collection<IndexInfo> indexInfos,
+            RowLogMessage message, RecordEvent recordEvent) {
+
+        try {
+            //
+            // Create the 'old' and 'new' Record instances.
+            //
+            // The RecordEvent contains the fields & record type info needed by the filters of the different
+            // indexes, this is taken care of by IndexSelectionRecordUpdateHook.
+            //
+            // Of course, it can happen that indexerconfs have been changed, or new indexes have been added,
+            // since the event was created, and then this information will be missing. We could check for that
+            // and in case of doubt send the event to the index anyway. This approach however also has the
+            // disadvantage that, in case there are a lot of outstanding events in the queue, that they
+            // might be sent to indexes that only expect a low update rate. Besides, it also complicates the
+            // code. So we go for the simple approach: when the indexerconfs change, there is a transition
+            // period to be expected, and one might need to rebuild indexes.
+            //
+            RecordId recordId = repository.getIdGenerator().fromBytes(message.getRowKey());
+            Record newRecord = repository.newRecord(recordId);
+            Record oldRecord = repository.newRecord(recordId);
+
+            RecordEvent.IndexSelection idxSel = recordEvent.getIndexSelection();
+            if (idxSel != null) {
+                TypeManager typeManager = repository.getTypeManager();
+
+                if (idxSel.getFieldChanges() != null) {
+                    for (RecordEvent.FieldChange fieldChange : idxSel.getFieldChanges()) {
+                        FieldType fieldType = typeManager.getFieldTypeById(fieldChange.getId());
+                        QName name = fieldType.getName();
+
+                        if (fieldChange.getNewValue() != null) {
+                            Object value = fieldType.getValueType().read(fieldChange.getNewValue());
+                            newRecord.setField(name, value);
+                        }
+
+                        if (fieldChange.getOldValue() != null) {
+                            Object value = fieldType.getValueType().read(fieldChange.getOldValue());
+                            oldRecord.setField(name, value);
+                        }
+                    }
+                }
+
+                if (idxSel.getNewRecordType() != null) {
+                    newRecord.setRecordType(typeManager.getRecordTypeById(idxSel.getNewRecordType(), null).getName());
+                }
+
+                if (idxSel.getOldRecordType() != null) {
+                    oldRecord.setRecordType(typeManager.getRecordTypeById(idxSel.getOldRecordType(), null).getName());
+                }
+            }
+
+            //
+            // And now, the actual subscription filtering
+            //
+            for (IndexInfo indexInfo : indexInfos) {
+                // If the filter of the indexerconf matches either the old or new record state,
+                // then the index needs to process this event
+                boolean relevantIndex = false;
+                IndexRecordFilter filter = indexInfo.getIndexerConf().getRecordFilter();
+                if (filter.getIndexCase(oldRecord) != null || filter.getIndexCase(newRecord) != null) {
+                    relevantIndex = true;
+                }
+
+                // If not relevant, remove it from the list of subscriptions
+                if (!relevantIndex) {
+                    String subscriptionId = indexInfo.getIndexDefinition().getQueueSubscriptionId();
+                    removeSubscription(subscriptions, subscriptionId);
+                }
+            }
+        } catch (Exception e) {
+            log.error("Error while performing index-aware filtering of subscriptions in the MQ feeder", e);
+        }
+    }
+
+    /**
+     * Add message to the MQ, this is the code from the original MessageQueueFeeder.
+     */
+    private boolean addMessageToMQ(RowLogMessage message, List<RowLogSubscription> subscriptions)
+            throws InterruptedException {
         Exception lastException = null;
         // When an exception occurs, we retry to put the message.
         // But only during 5 seconds since there can be a client waiting on its call to return.
@@ -188,16 +218,6 @@ public class IndexAwareMQFeeder implements RowLogMessageListener {
         log.info("Failed to put message '" + message + "' on the message queue. Retried during 5 seconds.",
                 lastException);
         return false;
-    }
-
-    private void removeSubscription(List<RowLogSubscription> subscriptions, String idToRemove) {
-        Iterator<RowLogSubscription> subscriptionIt = subscriptions.iterator();
-        while (subscriptionIt.hasNext()) {
-            if (subscriptionIt.next().getId().equals(idToRemove)) {
-                subscriptionIt.remove();
-                return;
-            }
-        }
     }
 }
 
