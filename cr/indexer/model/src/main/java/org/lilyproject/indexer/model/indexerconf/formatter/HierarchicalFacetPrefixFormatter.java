771eef3bff824a3ff6a51e16d123656b3f89a712
Bruno Dumon <bruno@ngdata.com>
2012-08-02 12:05:58 +0200

    Add a new formatter which translates paths to strings suitable for Solr hierarhical faceting.

---
+++ cr/indexer/model/src/main/java/org/lilyproject/indexer/model/indexerconf/formatter/HierarchicalFacetPrefixFormatter.java
@@ -0,0 +1,72 @@
+package org.lilyproject.indexer.model.indexerconf.formatter;
+
+import com.google.common.base.Splitter;
+import org.lilyproject.indexer.model.indexerconf.Formatter;
+import org.lilyproject.indexer.model.indexerconf.IndexValue;
+import org.lilyproject.repository.api.Repository;
+import org.lilyproject.repository.api.ValueType;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Formatter which formats values suitable for hierarchical faceting in Solr.
+ *
+ * <p>More information on hierarchical faceting in Solr can be found at
+ * https://wiki.apache.org/solr/HierarchicalFaceting</p>
+ *
+ * <p>To take the example for the Solr wiki page, the following hierarchies:</p>
+ *
+ * <pre>
+ * Doc#1: NonFic > Law
+ * Doc#2: NonFic > Sci
+ * Doc#3: NonFic > Hist, NonFic > Sci > Phys
+ * </pre>
+ *
+ * <p>are by this formatter translated to multiple tokens, prefixed with the
+ * depth of each path:</p>
+ *
+ * <pre>
+ * Doc#1: 0/NonFic, 1/NonFic/Law
+ * Doc#2: 0/NonFic, 1/NonFic/Sci
+ * Doc#3: 0/NonFic, 1/NonFic/Hist,
+ *           0/NonFic, 1/NonFic/Sci, 2/NonFic/Sci/Phys
+ * </pre>
+ *
+ * <p>So the resulting value of this formatter will (in most cases) be
+ * a multi-value. The input can be single or multi-valued. The inputs
+ * should be slash-separated strings.</p>
+ *
+ */
+public class HierarchicalFacetPrefixFormatter implements Formatter {
+    @Override
+    public List<String> format(List<IndexValue> indexValues, Repository repository) throws InterruptedException {
+        List<String> result = new ArrayList<String>();
+        for (IndexValue indexValue : indexValues) {
+            ValueType valueType = indexValue.fieldType.getValueType();
+            if (valueType.getBaseName().equals("LIST")) {
+                // The values of the first list-level are supplied as individual IndexValues
+                valueType = valueType.getNestedValueType();
+            }
+
+            if (!valueType.getName().equals("STRING")) {
+                throw new RuntimeException(this.getClass().getSimpleName() + " only supports string values, but got: "
+                        + valueType.getName());
+            }
+            processPath((String)indexValue.value, result);
+        }
+        return result;
+    }
+
+    private void processPath(String path, Collection<String> result) {
+        final Iterable<String> splitted = Splitter.on("/").omitEmptyStrings().trimResults().split(path);
+        int level = 0;
+        final StringBuilder entry = new StringBuilder();
+        for (String split : splitted) {
+            entry.append("/").append(split);
+            result.add(level + entry.toString());
+            level++;
+        }
+    }
+}
---
+++ cr/indexer/model/src/test/java/org/lilyproject/indexer/model/indexerconf/formatter/HierarchicalFacetPrefixFormatterTest.java
@@ -0,0 +1,54 @@
+package org.lilyproject.indexer.model.indexerconf.formatter;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+import org.lilyproject.indexer.model.indexerconf.IndexValue;
+import org.lilyproject.repository.api.FieldType;
+import org.lilyproject.repository.api.TypeManager;
+import org.lilyproject.repository.api.ValueType;
+import org.lilyproject.repository.impl.valuetype.ListValueType;
+import org.lilyproject.repository.impl.valuetype.StringValueType;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HierarchicalFacetPrefixFormatterTest {
+    @Test
+    public void testSingValue() throws Exception {
+        ValueType valueType = new StringValueType();
+        FieldType fieldType = mock(FieldType.class);
+        when(fieldType.getValueType()).thenReturn(valueType);
+
+        HierarchicalFacetPrefixFormatter formatter = new HierarchicalFacetPrefixFormatter();
+        IndexValue value = new IndexValue(null, fieldType, "foo/bar");
+        List<String> result = formatter.format(Lists.newArrayList(value), null);
+
+        assertEquals(2, result.size());
+        assertEquals("0/foo", result.get(0));
+        assertEquals("1/foo/bar", result.get(1));
+    }
+
+    @Test
+    public void testMultiValue() throws Exception {
+        TypeManager typeManager = mock(TypeManager.class);
+        when(typeManager.getValueType("STRING")).thenReturn(new StringValueType());
+
+        ValueType valueType = new ListValueType(typeManager, "STRING");
+        FieldType fieldType = mock(FieldType.class);
+        when(fieldType.getValueType()).thenReturn(valueType);
+
+        HierarchicalFacetPrefixFormatter formatter = new HierarchicalFacetPrefixFormatter();
+        List<String> result = formatter.format(Lists.newArrayList(
+                new IndexValue(null, fieldType, "foo/bar"),
+                new IndexValue(null, fieldType, "bar/foo")), null);
+
+        assertEquals(4, result.size());
+        assertEquals("0/foo", result.get(0));
+        assertEquals("1/foo/bar", result.get(1));
+        assertEquals("0/bar", result.get(2));
+        assertEquals("1/bar/foo", result.get(3));
+    }
+}
