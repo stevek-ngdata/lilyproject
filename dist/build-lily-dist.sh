772d12198bbbdef101a8c2af62fb8c3c67a4af82
Karel Vervaeke <karel@ngdata.com>
2012-09-20 18:01:38 +0200

    LILY-568 Fix the lib dir of the binary dist
    Before we were using dependencies of the 'dist' module to gather artifacts and their runtime dependencies (in conjunction with the mvn assembly plugin).
    Now we're using our own plugin to assemble artifacts and their transitive runtime dependencies. (The plugin is based on the library that's powering maven 3 - aether).
    
    Some side notes:
    - removed solr-core dependency - apparently this is no longer needed.
    - The maven version bump required adding the maven-compat project here and there.

--- dist/build-lily-dist.sh
+++ dist/build-lily-dist.sh
@@ -1,3 +1,7 @@
+set -e
+
 mvn -o org.lilyproject:lily-kauri-plugin:assemble-project-repository
 mvn -o org.lilyproject:lily-kauri-plugin:assemble-runtime-repository
+mvn -o org.lilyproject:lily-kauri-plugin:assemble-pom-repository
+
 mvn assembly:assembly
