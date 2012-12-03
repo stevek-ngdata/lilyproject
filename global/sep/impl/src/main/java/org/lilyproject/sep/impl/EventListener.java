76937a70d44bf94391a2265db9ba0cd4bf21d8af
Gabriel Reid <gabrielr@ngdata.com>
2012-12-03 12:14:30 +0100

    Add SEP event publishing to IndexUpdater

--- global/sep/impl/src/main/java/org/lilyproject/sep/impl/EventListener.java
+++ global/sep/impl/src/main/java/org/lilyproject/sep/impl/EventListener.java
@@ -1,5 +1,8 @@
 package org.lilyproject.sep.impl;
 
+/**
+ * Handles incoming Secondary Event Processor messages.
+ */
 public interface EventListener {
     boolean processMessage(byte[] row, byte[] payload);
 }
