d7b3894f6fa10edefc005fa761619a6d1be3e8fb
Jan Van Besien <janvanbesien@ngdata.com>
2012-12-05 10:32:04 +0100

    Refactored exception mapper in order to allow reuse in other exception mappers

--- cr/process/server/rest-module/src/main/java/org/lilyproject/rest/providers/json/GenericExceptionMapper.java
+++ cr/process/server/rest-module/src/main/java/org/lilyproject/rest/providers/json/GenericExceptionMapper.java
@@ -15,11 +15,10 @@
  */
 package org.lilyproject.rest.providers.json;
 
-import org.codehaus.jackson.node.ArrayNode;
-import org.codehaus.jackson.node.JsonNodeFactory;
-import org.codehaus.jackson.node.ObjectNode;
-import org.lilyproject.rest.ResourceException;
-import org.lilyproject.util.json.JsonFormat;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -27,49 +26,76 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+
+import org.codehaus.jackson.node.ArrayNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+import org.lilyproject.rest.ResourceException;
+import org.lilyproject.util.json.JsonFormat;
 
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
 
     @Override
     public Response toResponse(Throwable throwable) {
-        int status = 500;
-        Throwable mainThrowable = throwable;
-
         if (throwable instanceof ResourceException) {
-            ResourceException re = (ResourceException)throwable;
+            ResourceException re = (ResourceException) throwable;
+            return createResponseForResourceException(re);
+        } else {
+            return createGenericResponse(throwable);
+        }
+    }
 
-            status = re.getStatus();
+    private Response createResponseForResourceException(ResourceException re) {
+        int status = re.getStatus();
+        Throwable mainThrowable = re;
 
-            // If the exception only serves to annotate another exception with a status code...
-            if (re.getSpecificMessage() == null && re.getCause() != null) {
-                if (re.getCause() != null) {
-                    mainThrowable = re.getCause();
-                }
-            } else {
-                mainThrowable = throwable;
+        // If the exception only serves to annotate another exception with a status code...
+        if (re.getSpecificMessage() == null && re.getCause() != null) {
+            if (re.getCause() != null) {
+                mainThrowable = re.getCause();
             }
-
         }
 
+        return createResponse(status, mainThrowable);
+    }
+
+    private Response createGenericResponse(Throwable throwable) {
+        int status = 500;
+        return createResponse(status, throwable);
+    }
+
+    private Response createResponse(int status, Throwable mainThrowable) {
+        final ObjectNode msgNode = createJson(status, null);
+
+        addCausesAndStackTraces(mainThrowable, msgNode);
+
+        return jsonToResponse(status, msgNode);
+    }
+
+    public static ObjectNode createJson(int status, String providedDescription) {
         final ObjectNode msgNode = JsonNodeFactory.instance.objectNode();
         msgNode.put("status", status);
 
         Response.Status statusObject = Response.Status.fromStatusCode(status);
-        String description =  statusObject != null ? statusObject.toString() : null;
+        String description;
+        if (providedDescription == null)
+            description = (statusObject != null ? statusObject.toString() : null);
+        else
+            description = providedDescription;
 
         if (description != null) {
             msgNode.put("description", description);
         }
+        return msgNode;
+    }
 
+    public static void addCausesAndStackTraces(Throwable mainThrowable, ObjectNode msgNode) {
         msgNode.put("causes", causesToJson(mainThrowable));
-
         msgNode.put("stackTrace", formatStackTrace(mainThrowable));
+    }
 
+    public static Response jsonToResponse(int status, final ObjectNode msgNode) {
         StreamingOutput entity = new StreamingOutput() {
             @Override
             public void write(OutputStream output) throws IOException, WebApplicationException {
@@ -80,7 +106,7 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
         return Response.status(status).type(MediaType.APPLICATION_JSON_TYPE).entity(entity).build();
     }
 
-    private ArrayNode causesToJson(Throwable throwable) {
+    private static ArrayNode causesToJson(Throwable throwable) {
         ArrayNode causesNode = JsonNodeFactory.instance.arrayNode();
 
         while (throwable != null) {
@@ -94,7 +120,7 @@ public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
         return causesNode;
     }
 
-    private String formatStackTrace(Throwable throwable) {
+    private static String formatStackTrace(Throwable throwable) {
         StringWriter writer = new StringWriter();
         PrintWriter printWriter = new PrintWriter(writer);
         throwable.printStackTrace(printWriter);
