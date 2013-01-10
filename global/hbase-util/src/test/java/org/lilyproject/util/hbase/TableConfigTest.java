792e3d33bd954c82e1d6639d05930f20db4b6965
Jan Van Besien <janvanbesien@ngdata.com>
2013-01-10 10:40:16 +0100

    Some more key split tests for multi-byte patterns

--- global/hbase-util/src/test/java/org/lilyproject/util/hbase/TableConfigTest.java
+++ global/hbase-util/src/test/java/org/lilyproject/util/hbase/TableConfigTest.java
@@ -30,4 +30,32 @@ public class TableConfigTest {
         Assert.assertEquals(0x10, splitKeys[0][1]);
     }
 
+    @Test
+    public void testBinarySplitKeyParsing0F() {
+        final byte[][] splitKeys = new TableConfig(1, "\\x0F", new byte[]{}).getSplitKeys();
+        Assert.assertEquals(1, splitKeys.length);
+        Assert.assertEquals(1, splitKeys[0].length);
+        Assert.assertEquals(0xF, splitKeys[0][0]);
+    }
+
+    @Test
+    public void testBinarySplitKeyParsingF0() {
+        final byte[][] splitKeys = new TableConfig(1, "\\xF0", new byte[]{}).getSplitKeys();
+        Assert.assertEquals(1, splitKeys.length);
+        Assert.assertEquals(1, splitKeys[0].length);
+        Assert.assertEquals(0xF0, readUnsigned(splitKeys[0][0]));
+    }
+
+    @Test
+    public void testBinarySplitKeyParsingF0AB() {
+        final byte[][] splitKeys = new TableConfig(1, "\\xF0\\xAB", new byte[]{}).getSplitKeys();
+        Assert.assertEquals(1, splitKeys.length);
+        Assert.assertEquals(2, splitKeys[0].length);
+        Assert.assertEquals(0xF0, readUnsigned(splitKeys[0][0]));
+        Assert.assertEquals(0xAB, readUnsigned(splitKeys[0][1]));
+    }
+
+    private static int readUnsigned(byte value) {
+        return value & 0xFF;
+    }
 }
