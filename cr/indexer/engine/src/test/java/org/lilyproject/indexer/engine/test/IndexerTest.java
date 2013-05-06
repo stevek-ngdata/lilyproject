4cb011bd63b41bae1922c387daf58e17880c442b
Bruno Dumon <bruno@ngdata.com>
2013-05-06 12:44:30 +0200

    Fix missing getTenantName

--- cr/indexer/engine/src/test/java/org/lilyproject/indexer/engine/test/IndexerTest.java
+++ cr/indexer/engine/src/test/java/org/lilyproject/indexer/engine/test/IndexerTest.java
@@ -3428,6 +3428,11 @@ public class IndexerTest {
             return null;
         }
 
+        @Override
+        public String getTenantName() {
+            return delegate.getTenantName();
+        }
+
         public void reset() {
             for (TrackingTable repo : tableCache.values()) {
                 repo.reads();
