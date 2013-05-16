8a03c7361e8c4749efa01491606e34d0a396663f
Bruno Dumon <bruno@ngdata.com>
2013-05-16 13:59:23 +0200

    javadoc

--- cr/repository/spi/src/main/java/org/lilyproject/repository/spi/RepositoryDecoratorFactory.java
+++ cr/repository/spi/src/main/java/org/lilyproject/repository/spi/RepositoryDecoratorFactory.java
@@ -3,5 +3,12 @@ package org.lilyproject.repository.spi;
 import org.lilyproject.repository.api.Repository;
 
 public interface RepositoryDecoratorFactory {
+    /**
+     * This will be called once for each repository-table couple, the first time that that repository
+     * is requested.
+     *
+     * <p>In most cases, you will want to extend the actual decorator on {@link BaseRepositoryDecorator},
+     * passing the supplied delegate repository to its constructor.</p>
+     */
     Repository createInstance(Repository delegate);
 }
