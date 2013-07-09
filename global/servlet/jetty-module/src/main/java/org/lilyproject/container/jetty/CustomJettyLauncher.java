ed9fd9e0e4637867dfa4d478c3d2597ca320df31
Benny Colyn <bennyc@ngdata.com>
2013-07-09 17:12:35 +0200

    ssl support in lily jetty plugin

--- global/servlet/jetty-module/src/main/java/org/lilyproject/container/jetty/CustomJettyLauncher.java
+++ global/servlet/jetty-module/src/main/java/org/lilyproject/container/jetty/CustomJettyLauncher.java
@@ -15,43 +15,65 @@
  */
 package org.lilyproject.container.jetty;
 
+import static com.google.common.collect.Iterables.toArray;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import com.google.common.collect.Lists;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.lilyproject.runtime.rapi.ConfRegistry;
 import org.lilyproject.servletregistry.api.ServletFilterRegistryEntry;
 import org.lilyproject.servletregistry.api.ServletRegistry;
 import org.lilyproject.servletregistry.api.ServletRegistryEntry;
+import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+import org.mortbay.jetty.security.SslSocketConnector;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.FilterHolder;
 import org.mortbay.jetty.servlet.ServletHolder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 public class CustomJettyLauncher {
+
+    public static final String LILY_SSL_KEYSTORE = "lily.ssl.keystore";
 
     private Log log = LogFactory.getLog(CustomJettyLauncher.class);
 
     @Autowired
     private ServletRegistry servletRegistry;
 
+    @Autowired
+    private ConfRegistry confRegistry;
+
     private Server server;
     private Context context;
 
     public CustomJettyLauncher() throws Exception {
         log.trace("in constructor custom jetty launcher");
-
-        server = new Server(12060);
-
-        context = new Context(server, "/", Context.SESSIONS);
     }
 
     @PostConstruct
     public Server getServerInstance() throws Exception {
+        if (server != null) return server;
+
+        log.trace("starting jetty http server.");
+        server = new Server();
+        ArrayList<Connector> connectors = Lists.newArrayList();
+        addSSLConnector(connectors);
+        addPlainConnector(connectors);
+
+        server.setConnectors(toArray(connectors, Connector.class));
+        context = new Context(server, "/", Context.SESSIONS);
+
         for (ServletRegistryEntry entry: servletRegistry.getEntries()) {
             ServletHolder servletHolder = new ServletHolder(entry.getServletInstance(context.getServletContext()));
             for (String pattern: entry.getUrlPatterns()) {
@@ -75,4 +97,65 @@ public class CustomJettyLauncher {
         server.stop();
     }
 
+    private void addPlainConnector(List<Connector> connectors) {
+        SelectChannelConnector selectChannelConnector = new SelectChannelConnector();
+        selectChannelConnector.setPort(httpPort());
+        if (startSSL())
+            selectChannelConnector.setConfidentialPort(httpsPort());
+        connectors.add(selectChannelConnector);
+    }
+
+    private void addSSLConnector(List<Connector> connectors) {
+        if (!startSSL()) return;
+
+        SslSocketConnector sslConnector = new SslSocketConnector();
+        sslConnector.setPort(httpsPort());
+
+        if (StringUtils.hasText(keystore())){
+            sslConnector.setKeystore(keystore());
+            sslConnector.setTruststore(truststore());
+        }
+
+        sslConnector.setPassword(keystorePassword());
+        sslConnector.setKeyPassword(keyPassword());
+        sslConnector.setTrustPassword(truststorePassword());
+
+        connectors.add(sslConnector);
+    }
+
+    private int httpsPort() {
+        return confRegistry.getConfiguration("jetty").getChild("httpsPort").getValueAsInteger(12443);
+    }
+
+    private int httpPort() {
+        return confRegistry.getConfiguration("jetty").getChild("httpPort").getValueAsInteger(12060);
+    }
+
+    private boolean startSSL() {
+        return confRegistry.getConfiguration("jetty").getChild("ssl").getChild("startSSL").getValueAsBoolean(false);
+    }
+
+    private String keystore() {
+        String keystore = confRegistry.getConfiguration("jetty").getChild("ssl").getChild("keystore").getValue(null);
+        if (keystore == null){
+            keystore = System.getProperty(LILY_SSL_KEYSTORE);
+        }
+        return keystore;
+    }
+
+    private String truststore() {
+        return confRegistry.getConfiguration("jetty").getChild("ssl").getChild("truststore").getValue(keystore());
+    }
+
+    private String keystorePassword() {
+        return confRegistry.getConfiguration("jetty").getChild("ssl").getChild("keystorePassword").getValue("changeit");
+    }
+
+    private String truststorePassword() {
+        return confRegistry.getConfiguration("jetty").getChild("ssl").getChild("truststorePassword").getValue(keystorePassword());
+    }
+
+    private String keyPassword() {
+        return confRegistry.getConfiguration("jetty").getChild("ssl").getChild("keyPassword").getValue(keystorePassword());
+    }
 }
