cbda1f1a50f01fd5512a4bc6824f02063df897c0
Bruno Dumon <bruno@ngdata.com>
2013-10-29 13:07:47 +0100

    Fix NPE

--- cr/avro-api/src/main/java/org/lilyproject/avro/AvroConverter.java
+++ cr/avro-api/src/main/java/org/lilyproject/avro/AvroConverter.java
@@ -69,6 +69,9 @@ public class AvroConverter {
     }
 
     public AuthorizationContext convert(AvroAuthzContext authzContext) {
+        if (authzContext == null)
+            return null;
+
         Set<String> roles = new HashSet<String>();
         for (String role : authzContext.getRoles()) {
             roles.add(role);
