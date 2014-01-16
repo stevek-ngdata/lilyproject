0afb424868bfb731ccd68a332a17b943dbcef3db
Benny Colyn <bennyc@ngdata.com>
2014-01-16 11:11:49 +0100

    change in permission format

--- cr/repository/impl/src/main/java/org/lilyproject/repository/impl/AbstractRepositoryManager.java
+++ cr/repository/impl/src/main/java/org/lilyproject/repository/impl/AbstractRepositoryManager.java
@@ -20,25 +20,23 @@ import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
 import com.ngdata.lily.security.hbase.client.AuthEnabledHTable;
 import com.ngdata.lily.security.hbase.client.AuthorizationContextProvider;
 import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.lilyproject.util.hbase.LilyHBaseSchema;
-import org.lilyproject.util.hbase.RepoAndTableUtil;
-
-import org.lilyproject.repository.api.RepositoryUnavailableException;
-
-import com.google.common.collect.Maps;
 import org.lilyproject.repository.api.IdGenerator;
 import org.lilyproject.repository.api.LRepository;
 import org.lilyproject.repository.api.RecordFactory;
 import org.lilyproject.repository.api.Repository;
 import org.lilyproject.repository.api.RepositoryException;
 import org.lilyproject.repository.api.RepositoryManager;
+import org.lilyproject.repository.api.RepositoryUnavailableException;
 import org.lilyproject.repository.api.TypeManager;
 import org.lilyproject.repository.model.api.RepositoryModel;
+import org.lilyproject.util.hbase.LilyHBaseSchema;
 import org.lilyproject.util.hbase.LilyHBaseSchema.Table;
+import org.lilyproject.util.hbase.RepoAndTableUtil;
 import org.lilyproject.util.io.Closer;
 
 /**
@@ -64,7 +62,7 @@ public abstract class AbstractRepositoryManager implements RepositoryManager {
      * always visible by the Lily Data Repository.
      */
     private static final Set<String> DEFAULT_PERMISSIONS = ImmutableSet.of(
-            PERMISSION_APP_NAME + ":rw:column=qualifier:bin_starts_with:"
+            PERMISSION_APP_NAME + ":rw#column:qualifier:bin_starts_with:"
                     + Bytes.toStringBinary(new byte[]{LilyHBaseSchema.RecordColumn.SYSTEM_PREFIX}));
 
     /**
